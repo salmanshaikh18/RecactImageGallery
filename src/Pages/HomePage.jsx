f8d925b6425bb40295c82eba0e77fb6782ed570c
Salman <salmanshaikh18786@gmail.com>
2023-11-04 04:52:45 +0530

    styling and api logic

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ImageCard from '../components/ImageCard'
+import './HomePage.css'
 
 const HomePage = () => {
+    const [data, setData] = useState([])
+    
+    const getData = async () => {
+        const response = await fetch('https://api.slingacademy.com/v1/sample-data/photos?offset=5&limit=20')
+        console.log("response: ", response)
+        const { photos } = await response.json()
+        setData(photos)
+        console.log('photos: ', photos)
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
   return (
-    <div>HomePage</div>
+    <div id="parent">
+        {
+            data?.map((e,i)=>{
+                return <ImageCard id={e.id} imageUrl={e.url} key={i}/>
+              })
+        }
+    </div>
   )
 }
 
---
+++ src/components/ImageCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import './ImageCard.css'
+
+const ImageCard = ({ imageUrl, id }) => {
+    const navigate = useNavigate()
+
+    const redirectToDetailsPage = () => {
+        navigate(`/details/:${id}`)
+    }
+
+    return (
+        <div className='box'>
+            <div className="imgBox">
+                <img className='image' src={imageUrl} alt="image" onClick={redirectToDetailsPage} />
+            </div>
+
+        </div>
+    )
+}
+
+export default ImageCard
